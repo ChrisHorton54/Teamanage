32fa685fc3415378200a966f4a16408c24d4656c
ChrisHorton54 <chris.hortonis@hotmail.co.uk>
2015-02-07 23:13:31 +0000

    Signed-off-by: ChrisHorton54 <chris.hortonis@hotmail.co.uk>

--- js/index.js
+++ js/index.js
@@ -28,4 +28,60 @@ $(document).ready(function(){
             $(".fan-login").addClass("login-active");
         }, 200);
     });
+    
+    $(".close").click(function(){
+        if($(".management-login").hasClass("login-active")){
+            $(".management-login").removeClass("login-active");
+        }
+        
+        if($(".player-login").hasClass("login-active")){
+            $(".player-login").removeClass("login-active");
+        }
+        
+        if($(".fan-login").hasClass("login-active")){
+            $(".fan-login").removeClass("login-active");
+        }
+        
+        $(".login-mask").removeClass("mask-active");
+        
+        setTimeout(function(){
+            $(".login-mask").css("display","none");
+            $(".management-login").css("display","none");
+            $(".player-login").css("display","none");
+            $(".fan-login").css("display","none");
+            $(".management-sign-up-form").css("display","none");                 $(".management-form").css("display","block"); 
+            $(".management-form").removeClass("log-in-deactive");
+            $(".management-sign-up-form").removeClass("sign-up-active");
+            $(".fan-form").css("display","block"); 
+            $(".fan-form").removeClass("log-in-deactive");
+            $(".fan-sign-up-form").removeClass("sign-up-active");
+        }, 700);
+    });
+    
+    $(".management-sign-up").click(function(){
+        $(".management-form").addClass("log-in-deactive");
+        
+        setTimeout(function(){
+            $(".management-form").css("display","none");
+            $(".management-sign-up-form").css("display","block");
+        }, 700);
+        
+        setTimeout(function(){
+            $(".management-sign-up-form").addClass("sign-up-active");
+        }, 800);
+    });
+    
+    $(".fan-sign-up").click(function(){
+        $(".fan-form").addClass("log-in-deactive");
+        
+        setTimeout(function(){
+            $(".fan-form").css("display","none");
+            $(".fan-sign-up-form").css("display","block");
+        }, 700);
+        
+        setTimeout(function(){
+            $(".fan-sign-up-form").addClass("sign-up-active");
+        }, 800);
+    });
+    
 });
