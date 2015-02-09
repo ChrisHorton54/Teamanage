1a3f88a15684da2d778a441df0e91d440b804c15
ChrisHorton54 <chris.hortonis@hotmail.co.uk>
2015-02-09 20:15:43 +0000

    Signed-off-by: ChrisHorton54 <chris.hortonis@hotmail.co.uk>

---
+++ js/management/squad.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    playerAmount();
+});
+
+function playerAmount(){
+    var clubID = localStorage.getItem("clubID");
+    $.ajax({
+        url:"http://teamanage.co.uk/scripts/management/player_amount.php",
+        type: "POST",
+        data: {clubID : clubID},
+        success:function(data){
+            returnPlayerAmount(data);
+        }
+    }); 
+}
+
+function returnPlayerAmount(data){
+    $("#player_amount").html(data);
+}
